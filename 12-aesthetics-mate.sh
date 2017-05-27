ade72d2fef5295a8d5a8941198e00b4d6010592d
gronki <gronki@gmail.com>
2017-05-27 03:08:05 +0200

    wiecej ustawien

--- 12-aesthetics-mate.sh
+++ 12-aesthetics-mate.sh
@@ -1,16 +1,39 @@
 #!/usr/bin/env bash
 
-gsettings set org.mate.font-rendering antialiasing 'rgba'
-gsettings set org.mate.font-rendering hinting 'slight'
-gsettings set org.mate.font-rendering rgba-order 'rgb'
+sudo dnf install -y cairo-dock
+
+gsettings set org.mate.desktop.font-rendering antialiasing 'rgba'
+gsettings set org.mate.desktop.font-rendering hinting 'slight'
+gsettings set org.mate.desktop.font-rendering rgba-order 'rgb'
 
 gsettings set org.mate.NotificationDaemon theme 'Slider'
 
 gsettings set org.mate.screensaver idle-activation-enabled false
 gsettings set org.mate.screensaver lock-enabled false
 
-gsettings set org.mate.interface font-name "Cantarell 11"
-gsettings set org.mate.interface document-font-name "Noto Serif 11"
-gsettings set org.mate.interface monospace-font-name "Fira Mono 14"
-gsettings set org.mate.interface gtk-theme "Adwaita"
-gsettings set org.mate.interface icon-theme "Numix Circle"
+gsettings set org.mate.desktop.interface gtk-dialogs-use-header true
+gsettings set org.mate.desktop.interface use-custom-font true
+gsettings set org.mate.desktop.interface enable-animations true
+
+gsettings set org.mate.desktop.interface font-name "Cantarell 11"
+gsettings set org.mate.desktop.interface document-font-name "Noto Serif 11"
+gsettings set org.mate.desktop.interface monospace-font-name "Fira Mono 14"
+gsettings set org.mate.desktop.interface gtk-theme "Arc-Dark"
+gsettings set org.mate.desktop.interface icon-theme "Numix Circle"
+
+gsettings set org.mate.power-manager button-lid-ac 'nothing'
+gsettings set org.mate.power-manager button-lid-battery 'suspend'
+gsettings set org.mate.power-manager button-suspend 'nothing'
+gsettings set org.mate.power-manager button-power 'interactive'
+
+gsettings set org.mate.desktop.peripherals.touchpad natural-scroll true
+gsettings set org.mate.desktop.peripherals.touchpad horizontal-two-finger-scrolling true
+gsettings set org.mate.desktop.peripherals.touchpad horizontal-edge-scrolling false
+gsettings set org.mate.desktop.peripherals.touchpad vertical-edge-scrolling false
+gsettings set org.mate.desktop.peripherals.touchpad disable-while-typing true
+gsettings set org.mate.desktop.peripherals.touchpad tap-to-click false
+
+gsettings set org.mate.desktop.session.required-components windowmanager 'compiz'
+gsettings set org.mate.desktop.session.required-components dock 'cairo-dock'
+
+gsettings set org.mate.marco.general theme 'Arc-Dark'
