5a351304bdad0f3ace29ced9b27cca795e4bf0e8
gronki <gronki@gmail.com>
2017-05-27 05:33:32 +0200

    zmienne globalne

---
+++ 00-basics.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -ex
+
+sudo dnf install -y git PackageKit-command-not-found
+sudo dnf install -y dconf-editor yumex-dnf
+sudo -b updatedb
+sudo systemctl enable sshd
+
+cat - > /tmp/editor.sh <<EOF
+export EDITOR=nano
+EOF
+sudo mv /tmp/editor.sh /etc/profile.d/editor.sh
+
+cat - > /tmp/cflags.sh <<EOF
+export CFLAGS="-O3 -march=native -ffast-math"
+export FFLAGS="-O3 -march=native -ffast-math"
+export CXXFLAGS="-O3 -march=native -ffast-math"
+EOF
+sudo mv /tmp/cflags.sh /etc/profile.d/cflags.sh
--- 01-tools.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-
-sudo dnf install -y git PackageKit-command-not-found
-sudo dnf install -y dconf-editor yumex-dnf
-sudo -b updatedb
-sudo systemctl enable sshd
