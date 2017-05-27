4f2c41672fc50be52fe43e9b58162703c5104d84
gronki <gronki@gmail.com>
2017-05-27 04:40:33 +0200

    poprawki

--- 12-aesthetics-mate.sh
+++ 12-aesthetics-mate.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-sudo dnf install -y plank
-nohup plank &
+sudo dnf install -y plank compiz {simple-,}ccsm
 
 gsettings set org.mate.font-rendering antialiasing 'rgba'
 gsettings set org.mate.font-rendering hinting 'slight'
@@ -21,7 +20,7 @@ gsettings set org.mate.interface document-font-name "Noto Serif 11"
 gsettings set org.mate.interface monospace-font-name "Fira Mono 14"
 gsettings set org.mate.interface gtk-theme "Arc-Darker"
 gsettings set org.mate.Marco.general theme 'Arc-Darker'
-gsettings set org.mate.interface icon-theme "Numix Circle"
+gsettings set org.mate.interface icon-theme "Numix-Circle"
 
 gsettings set org.mate.power-manager button-lid-ac 'nothing'
 gsettings set org.mate.power-manager button-lid-battery 'suspend'
@@ -35,6 +34,11 @@ gsettings set org.mate.peripherals-touchpad vertical-edge-scrolling false
 gsettings set org.mate.peripherals-touchpad disable-while-typing true
 gsettings set org.mate.peripherals-touchpad tap-to-click false
 
+gsettings set org.mate.peripherals-mouse cursor-size 24
+gsettings set org.mate.peripherals-mouse cursor-theme 'Adwaita'
+
 gsettings set org.mate.session.required-components windowmanager 'compiz'
 gsettings set org.mate.session.required-components dock 'plank'
 gsettings set net.launchpad.plank.dock.settings:/net/launchpad/plank/docks/dock1/ theme 'Arc'
+
+sudo cp compizconfig.Default.ini /etc/compizconfig/Default.ini
