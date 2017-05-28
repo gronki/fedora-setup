c8d69e46bb78ce3b73067afa8bc6527fdbad133c
gronki <gronki@gmail.com>
2017-05-28 07:15:52 +0200

    sam wykrywa czy intel; poza tym ustawienia mpv i flasha

--- 10-codecs.sh
+++ 10-codecs.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 set -ex
 
-sudo dnf install -y libva{,-utils}
-sudo dnf install libva-intel-driver || echo show goes on
-sudo dnf install libva-vdpau-driver vdpauinfo mesa-vdpau-drivers || echo show goes on
+sudo dnf install -y libva{,-utils} vdpauinfo
 
 sudo dnf install -y gstreamer1-{libav,vaapi} gstreamer1-plugins{-base,-good{,-extras},-bad{-free{,-extras},-freeworld,-nonfree}}
 sudo dnf install -y gstreamer1-plugin-mpg123 mpg123-libs
+
 sudo dnf install -y mpv
+echo 'hwdec=auto' | sudo tee -a /etc/mpv/mpv.conf
+
 sudo dnf install -y gstreamer-{ffmpeg,plugins-good}
 
 sudo dnf config-manager --set-enabled fedora-cisco-openh264
-sudo dnf install -y openh264 gstreamer1-plugin-openh264 mozilla-openh264 xpra-codecs-freeworld
+sudo dnf install -y openh264 gstreamer1-plugin-openh264 mozilla-openh264
 
 sudo dnf install -y libde265
+
+if glxinfo | grep Device | grep Intel
+then
+    # intel graphics
+    sudo dnf install -y libva-intel-driver libvdpau-va-gl
+    echo 'export VDPAU_DRIVER=va_gl' | sudo tee -a /etc/environment
+else
+    # nvidia graphics
+    sudo dnf install -y libva-vdpau-driver mesa-vdpau-drivers
+fi
+
+sudo mkdir -p /etc/adobe
+echo "EnableLinuxHWVideoDecode=1" | sudo tee /etc/adobe/mms.cfg
+echo "OverrideGPUValidation=1" | sudo tee -a /etc/adobe/mms.cfg
