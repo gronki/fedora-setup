2c01bdf3d528c2449297ca67866ce9c982be5da1
gronki <gronki@gmail.com>
2017-05-31 02:28:08 +0200

    iraf

---
+++ 63-iraf.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+sudo dnf install -y tcsh ncurses libXmu ncurses-compat-libs xpa xorg-x11-fonts-misc
+
+IRAFBASE=/opt/iraf
+
+sudo rm -rfv $IRAFBASE
+sudo mkdir -p $IRAFBASE
+cd $IRAFBASE
+
+sudo curl -L ftp://iraf.noao.edu/iraf/v216/PCIX/iraf.lnux.x86_64.tar.gz -o irpk.tar.gz
+sudo tar xfz irpk.tar.gz
+sudo rm irpk.tar.gz
+
+sudo ./install --system --verbose \
+    --term      xgterm \
+    --root      $IRAFBASE \
+    --bindir    /usr/local/bin \
+    --cache     /tmp \
+    --imdir     /tmp
+
+sudo ln -s $IRAFBASE/vendor/x11iraf/bin.linux/xgterm /usr/local/bin/
+
+cd && mkiraf
+
+cat - | tee /tmp/run-iraf.sh <<EOF
+alias iraf='xgterm -bg \#1E2022 -fg \#d0d0d0 -cr \#e0e0e0 -title IRAF -fn 12x24 -e cl'
+EOF
+
+sudo mv /tmp/run-iraf.sh /etc/profile.d/
