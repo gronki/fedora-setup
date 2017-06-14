3c81567211bcfd43baea6ef3c6a9bb3f78331fdd
gronki <gronki@gmail.com>
2017-06-14 08:03:11 +0200

    latex script

---
+++ 50-latex.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo dnf install \
+texstudio bibutils \
+texlive-collection-{langpolish,langenglish,fontsrecommended,fontsextra,htmlxml,bibtexextra,publishers,science} \
+texlive-{antt,datetime2-polish,glossaries-polish,mwcls} \
+texlive-ams{cls,tex,math,refs} \
+texlive-{mnras,astro,biblatex-phys} \
+texlive-{fira,droid,markdown,fontspec,mathspec,ctablestack,tex4ht}
