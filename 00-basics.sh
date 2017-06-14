4be739f68d2224ffc95cb0688208f19f2c0904ee
gronki <gronki@gmail.com>
2017-06-14 08:25:45 +0200

    fixed newline issue

--- 00-basics.sh
+++ 00-basics.sh
@@ -7,14 +7,12 @@ sudo systemctl enable sshd
 
 echo 'export EDITOR=nano' | sudo tee -a /etc/profile.d/editor.sh
 
-OPT_FLAGS="-g -Wall -O3 -march=native -mieee-fp"
-FFLAGS_EXTRA="-Wstandard -Warray-temporaries -Wno-unused-dummy-argument
+CFLAGS="-g -Wall -O3 -march=native -mieee-fp"
+FFLAGS_EXTRA="-Wstandard -Warray-temporaries -Wno-unused-dummy-argument \
 -Waliasing -Wampersand -Wdeprecated -Wtarget-lifetime -Wsurprising"
-cat - > /tmp/cflags.sh <<EOF
-export CFLAGS="$OPT_FLAGS"
-export FFLAGS="$OPT_FLAGS $FFLAGS_EXTRA"
-export FCFLAGS="$OPT_FLAGS $FFLAGS_EXTRA"
-export F95FLAGS="$OPT_FLAGS $FFLAGS_EXTRA"
-export CXXFLAGS="$OPT_FLAGS"
-EOF
-sudo mv /tmp/cflags.sh /etc/profile.d/cflags.sh
+
+echo "export CFLAGS=\"$CFLAGS\"
+export FFLAGS=\"$CFLAGS $FFLAGS_EXTRA\"
+export FCFLAGS=\"$CFLAGS $FFLAGS_EXTRA\"
+export F95FLAGS=\"$CFLAGS $FFLAGS_EXTRA\"
+export CXXFLAGS=\"$CFLAGS\" " | sudo tee -a /etc/profile.d/cflags.sh
