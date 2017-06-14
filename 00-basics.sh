f94751aff7bf00e26c16927f0f544ba2989b6cf9
gronki <gronki@gmail.com>
2017-06-14 08:20:40 +0200

    different cflags

--- 00-basics.sh
+++ 00-basics.sh
@@ -5,14 +5,16 @@ sudo dnf install -y dconf-editor yumex-dnf
 sudo -b updatedb
 sudo systemctl enable sshd
 
-cat - > /tmp/editor.sh <<EOF
-export EDITOR=nano
-EOF
-sudo mv /tmp/editor.sh /etc/profile.d/editor.sh
+echo 'export EDITOR=nano' | sudo tee -a /etc/profile.d/editor.sh
 
+OPT_FLAGS="-g -Wall -O3 -march=native -mieee-fp"
+FFLAGS_EXTRA="-Wstandard -Warray-temporaries -Wno-unused-dummy-argument
+-Waliasing -Wampersand -Wdeprecated -Wtarget-lifetime -Wsurprising"
 cat - > /tmp/cflags.sh <<EOF
-export CFLAGS="-O3 -march=native -ffast-math"
-export FFLAGS="-O3 -march=native -ffast-math"
-export CXXFLAGS="-O3 -march=native -ffast-math"
+export CFLAGS="$OPT_FLAGS"
+export FFLAGS="$OPT_FLAGS $FFLAGS_EXTRA"
+export FCFLAGS="$OPT_FLAGS $FFLAGS_EXTRA"
+export F95FLAGS="$OPT_FLAGS $FFLAGS_EXTRA"
+export CXXFLAGS="$OPT_FLAGS"
 EOF
 sudo mv /tmp/cflags.sh /etc/profile.d/cflags.sh
