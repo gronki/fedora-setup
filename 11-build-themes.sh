7204dbf11dc58f1c6b049855faad48af5a4bd9c0
gronki <gronki@gmail.com>
2017-06-16 22:13:05 +0200

    bierzemy z mastera

--- 11-build-themes.sh
+++ 11-build-themes.sh
@@ -3,33 +3,38 @@
 set -ex
 
 prefix=/usr
-numix_frost_version=3.6.6
-pop_gtk_version=1.2.2
 
 # dependencies for POP & Numix Frost Theme
-sudo dnf install -y glib2-devel gdk-pixbuf2-devel
+sudo dnf install -y glib2-devel gdk-pixbuf2-devel rubygem-sass
 
-# numix frost
-pushd $(mktemp -d)
-curl -L https://github.com/Antergos/Numix-Frost/archive/$numix_frost_version.zip -o Numix-Frost.zip
-unzip Numix-Frost.zip
-cd Numix-Frost-$numix_frost_version
+# NUMIX FROST THEME
+
+cd $(mktemp -d)
+curl -L https://github.com/Antergos/Numix-Frost/archive/master.zip -o Numix-Frost.zip
+unzip Numix-Frost.zip && cd Numix-Frost-master
 make
 sudo make install prefix="$prefix"
-popd
 
 # dependencies for POP theme
 sudo dnf install -y librsvg2-devel parallel sassc
 
-# POP theme
-pushd $(mktemp -d)
-curl -L https://github.com/system76/pop-gtk-theme/archive/v$pop_gtk_version.zip -o pop-gtk-theme.zip
-unzip pop-gtk-theme.zip
-cd pop-gtk-theme-$pop_gtk_version
+# ADAPTA THEME
+
+cd $(mktemp -d)
+curl -L https://github.com/adapta-project/adapta-gtk-theme/archive/master.tar.gz -o adapta.tar.gz
+tar xzf adapta.tar.gz && cd adapta-gtk-theme-master
 ./autogen.sh --prefix="$prefix"
 make
 sudo make install
-popd
 
-sudo dnf remove -y glib2-devel gdk-pixbuf2-devel librsvg2-devel
-sudo dnf remove -y parallel sassc
+# POP THEME
+
+cd $(mktemp -d)
+curl -L https://github.com/system76/pop-gtk-theme/archive/master.zip -o pop-gtk-theme.zip
+unzip pop-gtk-theme.zip && cd pop-gtk-theme-master
+./autogen.sh --prefix="$prefix"
+make
+sudo make install
+
+sudo dnf remove -y glib2-devel gdk-pixbuf2-devel librsvg2-devel parallel
+
