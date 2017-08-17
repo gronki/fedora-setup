bb59f89e371c2922f061844d2e9fb159923495c4
Dominik Gronkiewicz <gronki@gmail.com>
2017-08-17 13:01:46 +0200

    telescope

--- 60-telescope.sh
+++ 60-telescope.sh
@@ -2,8 +2,8 @@
 set -ex
 
 # telescope control
-sudo dnf install -y libindi indi-eqmod indi-gphoto
-sudo dnf install kstars ccdciel
+sudo dnf install -y libindi indi-eqmod indi-gphoto wget
+sudo dnf install kstars ccdciel || echo nope
 
 # TheImagingSource
 
@@ -14,16 +14,20 @@ sudo curl -L \
 # ATIK
 
 sudo dnf install -y libnova
-cd /usr/lib64
-sudo ln -sf libnova-0.15.so.0 libnova-0.14.so.0
+sudo ln -sf $(rpm -E %_libdir)/libnova-0.15.so.0 \
+	$(rpm -E %_libdir)/libnova-0.14.so.0
 
 cd $(mktemp -d)
 if test $(arch) == armv7l
 then
-sudo dnf install -y alien
-wget http://download.cloudmakers.eu/atikccd-1.23-armhf.deb
-alien -r atikccd-1.23-armhf.deb
+	sudo dnf install -y alien
+	wget http://download.cloudmakers.eu/atikccd-1.23-armhf.deb
+	sudo alien -t atikccd-1.23-armhf.deb
+	tar xzf atikccd-1.23.tgz
+	cd usr
+	sudo cp -r * /usr/local/
+	sudo dnf remove alien
 else
-wget http://download.cloudmakers.eu/atikccd-1.23-amd64.rpm
+	wget http://download.cloudmakers.eu/atikccd-1.23-amd64.rpm
+	sudo rpm -ivh --nodeps --force *.rpm
 fi
-sudo rpm -ivh --nodeps --force *.rpm
