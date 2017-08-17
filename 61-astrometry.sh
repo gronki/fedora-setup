39cfd7b5dfb2c58d06ea1f4454ebba61bbf6719d
Dominik Gronkiewicz <gronki@gmail.com>
2017-08-17 11:46:40 +0200

    astrometry

--- 61-astrometry.sh
+++ 61-astrometry.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
+set -e
 
-sudo dnf install -y {freetype,zlib,libpng,cairo,libjpeg-turbo,libimagequant}-devel \
-    swig redhat-rpm-config netpbm-progs
+sudo dnf install -y \
+    {freetype,zlib,libpng,cairo,cfitsio,libjpeg-turbo,libimagequant}-devel \
+    swig redhat-rpm-config netpbm-progs @c-development cfitsio \
+    python2-{numpy,devel}
 
 . /etc/profile.d/cflags.sh
 
 cd $(mktemp -d)
-ASTROMETRY_VERSION=0.70
+ASTROMETRY_VERSION=0.72
 curl -L \
     https://github.com/dstndstn/astrometry.net/archive/${ASTROMETRY_VERSION}.tar.gz \
     -o astrometry.net.tar.gz
@@ -25,5 +28,5 @@ sudo mv /tmp/astrometry-path.sh /etc/profile.d/
 cd /opt/astrometry/data
 sudo wget --continue http://broiler.astrometry.net/~dstn/4200/index-{4219,4218,4217,4216,4215,4214,4213,4212,4211,4210,4209,4208,{4207,4206}-{00,01,02,03,04,05,06,07,08,07,10,11}}.fits
 
-sudo dnf remove swig {freetype,zlib,libpng,cairo,libjpeg-turbo,libimagequant}-devel \
+sudo dnf remove swig {freetype,zlib,libpng,cairo,libjpeg-turbo,cfitsio,libimagequant}-devel \
         || echo nope
