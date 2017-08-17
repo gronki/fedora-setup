350e0f95a0ead57cb7e3e08261e0370107d8318e
gronki <gronki@gmail.com>
2017-08-17 13:36:01 +0200

    xxx

--- 00-basics.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-
-sudo yum install vim-enhanced
-echo export EDITOR=vim | sudo tee -a /etc/profile.d/editor.sh
-
-echo alias wifi=\'nmcli device wifi\' | sudo tee -a /etc/profile.d/aliases.sh
--- 61-astrometry.sh
+++ 61-astrometry.sh
@@ -15,9 +15,8 @@ curl -L \
     -o astrometry.net.tar.gz
 tar xzf astrometry.net.tar.gz
 cd astrometry.net-${ASTROMETRY_VERSION}
-./configure --prefix=/opt
-make -j $(nproc) all
-sudo make install
+./configure && make -j $(nproc)
+sudo make install INSTALL_DIR=/opt/astrometry
 
 cat - > /tmp/astrometry-path.sh <<EOF
 export PATH="\$PATH:/opt/astrometry/bin"
@@ -25,7 +24,8 @@ EOF
 
 sudo mv /tmp/astrometry-path.sh /etc/profile.d/
 
-cd /opt/astrometry/data
+mkdir -p /opt/astrometry/data && cd /opt/astrometry/data
+
 sudo wget --continue http://broiler.astrometry.net/~dstn/4200/index-{4219,4218,4217,4216,4215,4214,4213,4212,4211,4210,4209,4208,{4207,4206}-{00,01,02,03,04,05,06,07,08,07,10,11}}.fits
 
 sudo dnf remove swig {freetype,zlib,libpng,cairo,libjpeg-turbo,cfitsio,libimagequant}-devel \
