2a8a5a6c30443371b9733f0910ae3f7e246f245d
Dominik Gronkiewicz <gronki@gmail.com>
2017-08-19 18:05:22 +0200

    male zmiany

--- 61-openphd.sh
+++ 61-openphd.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 set -ex
 
-sudo dnf install -y @c-development cmake pkgconfig \
+sudo dnf install @c-development cmake pkgconfig \
     {wxGTK3,libindi,libnova,zlib,libusb}{,-devel} libindi-static
 
 . /etc/profile.d/cflags.sh
 
 OPENPHD_VER=2.6.3dev4
-pushd $(mktemp -d)
+
+cd $(mktemp -d)
 curl -L https://github.com/OpenPHDGuiding/phd2/archive/v${OPENPHD_VER}.zip -o phd2.zip
 unzip phd2.zip
+
 cd phd2-${OPENPHD_VER}
 mkdir -p tmp
 cd tmp
+
 cmake -DCMAKE_INSTALL_PREFIX=/usr/local ..
 make -j $(nproc)
 sudo make install
-
-popd
 
 sudo dnf remove wxGTK3-devel {libindi,libnova,zlib,libusb}-devel libindi-static || echo nope
