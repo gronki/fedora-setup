29dbeb1bc80d67b0af29ec4bcda9cd0ccc1997e6
gronki <gronki@gmail.com>
2017-08-21 21:18:47 +0200

    cflags

--- 01-cflags.sh
+++ 01-cflags.sh
@@ -3,12 +3,12 @@ set -ex
 
 T=$(mktemp)
 # rpm -E %optflags | tee $T
-echo -g -Wall -O2 -march=native | tee $T
+echo "-g -Wall -O2 -march=native" | tee $T
 vim $T
 CFLAGS=$(cat $T)
 
-echo export CFLAGS=\"$CFLAGS\" | sudo tee /etc/profile.d/cflags.sh
-echo export CXXFLAGS=\"$CFLAGS\" | sudo tee -a /etc/profile.d/cflags.sh
-echo export FFLAGS=\"$CFLAGS\" | sudo tee -a /etc/profile.d/cflags.sh
-echo export FCFLAGS=\"$CFLAGS\" | sudo tee -a /etc/profile.d/cflags.sh
-echo export F95FLAGS=\"$CFLAGS\" | sudo tee -a /etc/profile.d/cflags.sh
+echo "export CFLAGS=\"$CFLAGS\""   | sudo tee    /etc/profile.d/cflags.sh
+echo "export CXXFLAGS=\"$CFLAGS\"" | sudo tee -a /etc/profile.d/cflags.sh
+echo "export FFLAGS=\"$CFLAGS\""   | sudo tee -a /etc/profile.d/cflags.sh
+echo "export FCFLAGS=\"$CFLAGS\""  | sudo tee -a /etc/profile.d/cflags.sh
+echo "export F95FLAGS=\"$CFLAGS\"" | sudo tee -a /etc/profile.d/cflags.sh
