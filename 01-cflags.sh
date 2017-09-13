7b739ff0ea3b8a4a63d39c836fac3f4d6c98c40e
gronki <gronki@gmail.com>
2017-09-13 03:10:27 +0200

    cflags

--- 01-cflags.sh
+++ 01-cflags.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -ex
 
-T=$(mktemp)
-rpm --rcfile /usr/lib/rpm/rpmrc -E %optflags | tee $T
-vim $T
-CFLAGS=$(cat $T)
+sudo dnf install @c-development gcc-{c++,gfortran} redhat-rpm-config
 
-echo "export CFLAGS=\"$CFLAGS\""   | sudo tee    /etc/profile.d/cflags.sh
-echo "export CXXFLAGS=\"$CFLAGS\"" | sudo tee -a /etc/profile.d/cflags.sh
-echo "export FFLAGS=\"$CFLAGS\""   | sudo tee -a /etc/profile.d/cflags.sh
-echo "export FCFLAGS=\"$CFLAGS\""  | sudo tee -a /etc/profile.d/cflags.sh
-echo "export F95FLAGS=\"$CFLAGS\"" | sudo tee -a /etc/profile.d/cflags.sh
+cat | sudo tee /etc/profile.d/cflags.sh <<EOF
+OPTFLAGS="$(rpm --define '__global_compiler_flags -g -Wall -O2' -E %optflags)"
+
+# for rpi3
+# OPTFLAGS="-O2 -g -mcpu=cortex-a53 -mfloat-abi=hard -mfpu=neon-fp-armv8 -ffast-math"
+
+export CFLAGS="\$OPTFLAGS"
+export CXXLAGS="\$OPTFLAGS"
+export FFLAGS="\$OPTFLAGS -I $(rpm -E %_fmoddir)"
+export FCFLAGS="\$OPTFLAGS -I $(rpm -E %_fmoddir)"
+EOF
+
