0becf69f5afc3096fccaaa6ca31a287e71c8e1ea
gronki <gronki@gmail.com>
2018-02-10 08:28:33 +0100

    kilka paczek latexa

--- 50-latex.sh
+++ 50-latex.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo dnf install bibutils \
+sudo dnf install -y bibutils \
 texlive-collection-{langpolish,langenglish,science} \
 texlive-{antt,datetime2-polish,glossaries-polish,mwcls} \
 texlive-ams{cls,tex,math,refs} \
 texlive-{mnras,astro,biblatex-phys} \
-texlive-{epsf,ctablestack,tex4ht}
-sudo dnf install texstudio
+texlive-{epsf,ctablestack,tex4ht,sttools}
+sudo dnf install -y texstudio
