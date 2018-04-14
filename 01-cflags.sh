5ee0bf514306eec6e5a9c4d239783c37f2437445
gronki <gronki@gmail.com>
2018-04-14 23:39:28 +0200

    poprawka

--- 01-cflags.sh
+++ 01-cflags.sh
@@ -4,16 +4,16 @@ set -ex
 sudo dnf install @c-development gcc-{c++,gfortran} redhat-rpm-config
 
 cat | sudo tee /etc/profile.d/cflags.sh <<EOF
-OPTFLAGS="-g -Wall -O2 -march=native"
+OPTFLAGS="-O2 -ftree-vectorize -march=native"
 
 # rpm flags
-# OPTFLAGS="$(rpm --define '__global_compiler_flags -g -Wall -O2' -E %optflags)"
+# OPTFLAGS="$(rpm --define '__global_compiler_flags -O2' -E %optflags)"
 
 # rpm flags -- full (without hardening)
 # OPTFLAGS="$(rpm --undefine _hardened_build -E %optflags)"
 
 # for rpi3
-# OPTFLAGS="-g -O3 -Wall -mcpu=cortex-a53 -mfpu=neon-fp-armv8 -mfloat-abi=hard"
+# OPTFLAGS="-O2 -ftree-vectorize -pipe -mcpu=cortex-a53 -mfpu=neon-fp-armv8 -mfloat-abi=hard"
 
 export CFLAGS="\$OPTFLAGS"
 export CXXLAGS="\$OPTFLAGS"
