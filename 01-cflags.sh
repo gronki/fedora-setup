514ccbc479564b397c2dfe81866a38f97260f628
gronki <gronki@gmail.com>
2018-10-27 00:38:15 +0200

    raspberry pi detect

--- 01-cflags.sh
+++ 01-cflags.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-set -ex
+set -e
+
+OPTFLAGS="-g -Wall -O2"
+ARCH_FLAGS="-march=native"
+
+if cat /proc/cpuinfo | grep BCM2835; then
+	ARCH_FLAGS="-mcpu=cortex-a53 -mfpu=neon-fp-armv8 -mfloat-abi=hard"
+	OPTFLAGS="$OPTFLAGS -pipe"
+fi
 
 sudo tee /etc/profile.d/cflags.sh <<EOF
-ARCH_FLAGS="-march=native"
-OPTFLAGS="-O2 -ftree-vectorize \$ARCH_FLAGS"
+export ARCH_FLAGS="$ARCH_FLAGS"
+OPTFLAGS="$OPTFLAGS"
 
-# for rpi3
-# ARCH_FLAGS="-mcpu=cortex-a53 -mfpu=neon-fp-armv8 -mfloat-abi=hard"
-# OPTFLAGS="-O2 -ftree-vectorize -pipe \$ARCH_FLAGS"
-
-export CFLAGS="\$OPTFLAGS"
-export CXXFLAGS="\$OPTFLAGS"
-export FFLAGS="\$OPTFLAGS"
-export ARCH_FLAGS
+export   CFLAGS="\$OPTFLAGS \$ARCH_FLAGS"
+export CXXFLAGS="\$OPTFLAGS \$ARCH_FLAGS"
+export   FFLAGS="\$OPTFLAGS \$ARCH_FLAGS"
 EOF
 
 sudo vim /etc/profile.d/cflags.sh
