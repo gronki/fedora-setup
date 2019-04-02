be2a59f51c8e70681f992752765c16271cae42e8
gronki <gronki@gmail.com>
2019-04-02 19:37:23 +0200

    opera

--- 20-opera-ffmpeg.sh
+++ 20-opera-ffmpeg.sh
@@ -5,8 +5,10 @@ if [ $(arch) != x86_64 ]; then
 	exit -1
 fi
 
-ffmpeg_version=0.32.1
-wget "https://github.com/iteufel/nwjs-ffmpeg-prebuilt/releases/download/$ffmpeg_version/$ffmpeg_version-linux-x64.zip" -O ffmpeg.zip; unzip ffmpeg.zip
+ffmpeg_version=0.31.4
+wget "https://github.com/iteufel/nwjs-ffmpeg-prebuilt/releases/download/$ffmpeg_version/$ffmpeg_version-linux-x64.zip" -O ffmpeg.zip || exit -1
+unzip ffmpeg.zip
+
 sudo install -d /usr/lib64/ffmpeg-stolen
 sudo install libffmpeg.so /usr/lib64/ffmpeg-stolen/
 sudo ldconfig /usr/lib64/ffmpeg-stolen
@@ -14,5 +16,8 @@ sudo ldconfig /usr/lib64/ffmpeg-stolen
 rm -f ffmpeg.zip libffmpeg.so
 
 json_config_file=/usr/lib64/opera/resources/ffmpeg_preload_config.json
+if ! cat $json_config_file | grep ffmpeg-stolen; then
 cat $json_config_file | sed 's/\[/[\n  "\/usr\/lib64\/ffmpeg-stolen\/libffmpeg.so",/' | sudo tee $json_config_file
+fi
 
+opera --new-window youtube.com/html5
