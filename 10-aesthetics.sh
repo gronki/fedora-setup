7270ca4c110841aedeb6bc5871acc3fe98f87516
gronki <gronki@gmail.com>
2020-02-05 01:18:51 +0100

    removed old stuff

--- 10-aesthetics.sh
+++ 10-aesthetics.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 set -ex
 
-# subpixel rendering
-sudo dnf install -y freetype-freeworld
-
-# font configuration
-cd /etc/fonts/conf.d/
-sudo rm -f 10-hinting-*.conf
-sudo rm -f 10*sub-pixel*.conf
-sudo rm -f 11-lcdfilter-*.conf
-sudo ln -sf /usr/share/fontconfig/conf.avail/10-hinting-slight.conf
-sudo ln -sf /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf
-sudo ln -sf /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf
-
 # interface fonts
 sudo dnf install -y google-roboto-fonts \
     google-noto-{mono,sans,serif}-fonts \
