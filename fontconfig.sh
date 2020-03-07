3c43105fcb763e9605ae3518a7033ec303c33af1
gronki <gronki@gmail.com>
2020-03-08 00:14:02 +0100

    fontconfig

--- fontconfig.sh
+++ fontconfig.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+#-----------------------------------------------------------------------------#
+
 confd=/etc/fonts/conf.d
 availd=/usr/share/fontconfig/conf.avail
 Xres=/etc/X11/Xresources
@@ -14,15 +16,43 @@ setXres() {
 	echo "${1}.${2}: ${3}" | xrdb -merge
 }
 
+
+setHinting() {
+	hinting="$1"
+	sudo rm -fv "${confd}"/10-hinting-*.conf
+	sudo ln -s "${availd}/10-hinting-${hinting}.conf" "${confd}/10-hinting-${hinting}.conf"
+	setXres Xft hintstyle "hint${hinting}"
+	xfconf-query -c xsettings -p /Xft/HintStyle -s "hint${hinting}" || echo xfconf fail
+	gsettings set org.gnome.settings-daemon.plugins.xsettings hinting ${hinting} || echo gsettings fail
+	echo
+	echo -e "hinting set to \033[1m$hinting\033[0m"
+	echo
+}
+
+setLcdFilter() {
+	filter="$1"
+	sudo rm -fv "${confd}"/11-lcdfilter-*.conf
+	sudo ln -s "${availd}/11-lcdfilter-${filter}.conf" "${confd}/11-lcdfilter-${filter}.conf"
+	setXres Xft lcdfilter "lcd${filter}"
+	echo
+	echo -e "lcdfilter set to \033[1m$filter\033[0m"
+	echo
+}
+
+#-----------------------------------------------------------------------------#
+
+sudo pwd
+
+setXres Xft hinting "true"
+setXres Xft antialias "true"
+setXres Xft autohint "false"
+echo
+
 echo select hinting
 select hinting in slight medium full none
 do
 	if [ -n "$hinting" ]; then
-		sudo rm -fv "${confd}"/10-hinting-*.conf
-		sudo ln -s "${availd}/10-hinting-${hinting}.conf" "${confd}/10-hinting-${hinting}.conf"
-		setXres Xft hintstyle "hint${hinting}"
-		which xfconf-query && xfconf-query -c xsettings -p /Xft/HintStyle -s "hint${hinting}"
-		echo "hinting set to $hinting"
+		setHinting "$hinting"
 		break
 	fi
 done
@@ -31,12 +61,17 @@ echo select filter
 select filter in default light legacy
 do
 	if [ -n "$filter" ]; then
-		sudo rm -fv "${confd}"/11-lcdfilter-*.conf
-		sudo ln -s "${availd}/11-lcdfilter-${filter}.conf" "${confd}/11-lcdfilter-${filter}.conf"
-		setXres Xft lcdfilter "lcd${filter}"
-		echo "lcdfilter set to $filter"
+		setLcdFilter "$filter"
 		break
 	fi
 done
 
+#-----------------------------------------------------------------------------#
+
 fc-cache -r &
+
+#-----------------------------------------------------------------------------#
+
+# rgb bgr vbgr vrgb
+# /org/gnome/settings-daemon/plugins/xsettings/
+# xfconf-query -c xsettings -l -v
