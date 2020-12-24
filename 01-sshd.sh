779a7b56fbfe81bc1b5236f39f1d9f4cec3f9c1b
gronki <gronki@gmail.com>
2020-12-24 16:37:30 +0100

    deleted some stuff

--- 01-sshd.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ex
-
-sudo yum install -y openssh-server
-sudo systemctl enable sshd
--- 50-development.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -ex
-
-sudo dnf install -y \
-	@c-development @development-tools @rpm-development-tools \
-	fedpkg rpmdevtools copr-cli gists \
-	gcc-gfortran coffee-script
