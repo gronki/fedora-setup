f099bfdd94306f038f02ddd4a4c0661aabdfed07
gronki <gronki@gmail.com>
2020-12-27 16:03:36 +0100

    urxvt

--- 02-astro-pi.sh
+++ 02-astro-pi.sh
@@ -1,6 +1,6 @@
 sudo dnf install \
 			@base-x openbox obconf rofi tint2 tigervnc-server \
-		 	urxvt pcmanfm xdg-utils usbutils \
+		 	rxvt-unicode pcmanfm xdg-utils usbutils \
 	 		neovim htop ImageMagick \
 			kstars phd2 fpack siril \
 			{cfitsio,fftw3}{,-devel} \
