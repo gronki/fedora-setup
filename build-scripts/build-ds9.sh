74933a9f70f712cbbaec134767ea9a8ef7806f12
gronki <gronki@gmail.com>
2020-12-27 03:54:22 +0100

    trying to fix ds9

--- build-scripts/build-ds9.sh
+++ build-scripts/build-ds9.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-version=8.0.1
+version=8.2
 
 prefix=/usr/local
 bindir=$prefix/bin
@@ -10,18 +10,16 @@ libexecdir=$prefix/libexec
 datadir=$prefix/share
 docdir=$datadir/doc
 
-sudo dnf install -y {libX11,zlib,libxml2,libxslt,libXft,fontconfig}-devel \
-        libX11 zlib libxml2 libxslt fontconfig libXft tcl
+sudo dnf install -y {libX11,zlib,libxml2,libxslt,libXft,fontconfig}{,-devel} \
+        tcl perl-File-Compare
 sudo dnf install -y --allowerasing compat-openssl10-devel
 
 builddir=$(mktemp -d)
-cp ds9/saods9.{png,desktop} $builddir
+cp ../ds9/saods9.{png,desktop} $builddir
 cd $builddir
 
-curl -L http://ds9.si.edu/download/source/ds9.${version}.tar.gz -o saods9.tar.gz
-# curl -L https://github.com/wjoye/tkblt/archive/v${tkblt_version}.tar.gz -o tkblt.tar.gz
+curl -L http://ds9.si.edu/download/source/ds9.${version}.tar.gz | tar xzf -
 
-tar xzfv saods9.tar.gz && rm -fv saods9.tar.gz
 cd SAOImageDS9 
 
 unix/configure --prefix $prefix \
@@ -38,12 +36,11 @@ sudo install -d ${datadir}/{icons,applications}
 sudo install -d ${docdir}/saods9
 
 sudo install bin/ds9 ${bindir}
-sudo cp -rv ds9/doc/* ${docdir}/saods9/
+sudo install -m 644 ds9/doc/* ${docdir}/saods9/
 
 cd ..
 sudo install -m 644 saods9.png ${datadir}/icons/
 sudo install -m 644 saods9.desktop ${datadir}/applications/
-rm -rfv saods9
 
 sudo dnf remove {libX11,zlib,openssl,libxml2,libxslt,libXft,fontconfig}-devel
 sudo dnf remove compat-openssl10-devel
