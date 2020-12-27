7ec7a6e4065c82f9d9dab64353e0b013451df7d1
gronki <gronki@gmail.com>
2020-12-27 16:00:33 +0100

    astro pi

---
+++ 02-astro-pi.sh
@@ -0,0 +1,10 @@
+sudo dnf install \
+			@base-x openbox obconf rofi tint2 tigervnc-server \
+		 	urxvt pcmanfm xdg-utils usbutils \
+	 		neovim htop ImageMagick \
+			kstars phd2 fpack siril \
+			{cfitsio,fftw3}{,-devel} \
+			libnova python3-astropy
+
+
+
--- 02-minimal-desktop.sh
+++
@@ -1,3 +0,0 @@
-sudo dnf install openbox obconf @base-x tint2 \
-		 	urxvt rofi pcmanfm xdg-utils \
-	 		neovim htop ImageMagick
