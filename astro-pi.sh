039c23e8aa82c7d668a1af29154c97fa7081940e
Dominik Gronkiewicz <gronki@gmail.com>
2020-12-27 16:36:22 +0100

    astro pi runs other stripts

---
+++ astro-pi.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set +e
+set -x
+
+bash 01-cflags.sh
+
+sudo dnf install -y \
+	openbox obconf rofi tint2 tigervnc-server \
+	rxvt-unicode pcmanfm xdg-utils usbutils \
+	neovim htop ImageMagick
+sudo dnf install -y kstars phd2 fpack
+
+bash build-tools.sh
+
+sudo dnf install -y {cfitsio,fftw3}{,-devel}
+
+bash 60-telescope.sh
+
+echo 'remember to install or build astrometry, indi and ds9'
